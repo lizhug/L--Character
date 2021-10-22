1b934890606dac0fe111113030c05785be7a7ab0
李珠刚 <lizhug.steven@gmail.com>
2021-10-22 13:40:16 +0800

    更新

--- Assets/Scripts/UIJoysticks.cs
+++ Assets/Scripts/UIJoysticks.cs
@@ -5,29 +5,53 @@ using UnityEngine;
 public class UIJoysticks : MonoBehaviour
 {
     public Vector3 stickInitPosition;       // –Èƒ‚ª¨∏À≥ı ºªØŒª÷√
+	public RectTransform selfTransform;
+	public Vector3 originPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         stickInitPosition = GetComponentInParent<RectTransform>().position;
+		
+		selfTransform = GetComponentInParent<RectTransform>();
+		// originPosition = selfTransform.anchoredPosition;
+		
+		Debug.Log(11111);
+		Debug.Log(stickInitPosition);
+		Debug.Log(22222);
+		Debug.Log(originPosition);
+		Debug.Log(Input.mousePosition);
     }
 
     // Update is called once per frame
     void Update()
     { 
-   
+		//Debug.Log(transform.anchoredPosition);
     }
 
     // ”√ªß¥•≈ˆª¨∏À “∆∂Ø
     public void OnDragIng()
     {
-        transform.position = Input.mousePosition;
+		// if (originPosition) {
+		// 	originPosition = Input.mousePosition;
+		// }
+		
+        Vector3 offsetPosition =  Input.mousePosition - originPosition;
+		
+		Debug.Log(offsetPosition);
+		
+		// transform.position = transform.position + offsetPosition * Time.deltaTime;
 
-        Debug.Log(Input.mousePosition);
+		// Debug.Log(333333);
+		// Debug.Log(Input.mousePosition);
+		
+		// Debug.Log(44444);
+  //       Debug.Log(transform.position);
+		
     }
 
     public void OnDragEnd()
     {
-        //transform.position = stickInitPosition;
+        transform.position = stickInitPosition;
     }
 }
